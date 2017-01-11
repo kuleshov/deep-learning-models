256d59f7ad7e356d001f6cc312b01e7625137989
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2017-01-11 11:01:05 -0800

    GaussianMultiSampleLayer

--- models/layers/sampling.py
+++ models/layers/sampling.py
@@ -22,6 +22,35 @@ class GaussianSampleLayer(lasagne.layers.MergeLayer):
             return mu
         return mu + T.exp(logsigma) * self.rng.normal(shape)
 
+class GaussianMultiSampleLayer(lasagne.layers.MergeLayer):
+    def __init__(self, mu, logsigma, n_samples, rng=None, **kwargs):
+        self.rng = rng if rng else RandomStreams(lasagne.random.get_rng().randint(1,2147462579))
+        self.n_samples = n_samples
+        super(GaussianMultiSampleLayer, self).__init__([mu, logsigma], **kwargs)
+
+    def get_output_shape_for(self, input_shapes):
+        n_batch, n_dim = input_shapes[0]
+        return (n_batch, self.n_samples, n_dim)
+
+    def get_output_for(self, inputs, deterministic=False, **kwargs):
+        mu, logsigma = inputs
+
+        # get dimensions
+        n_bat = self.input_shapes[0][0] or inputs[0].shape[0]
+        n_dim = self.input_shapes[0][1] or inputs[0].shape[1]
+        n_sam = self.n_samples
+
+        # reshape inputs into rank-3 tensors
+        mu3 = mu.reshape((n_bat,1,n_dim)).repeat(n_sam, axis=1)
+        ls3 = logsigma.reshape((n_bat,1,n_dim)).repeat(n_sam, axis=1)
+        
+        # return reshape means if layer is deterministic
+        if deterministic: return mu3
+
+        # otherwise, take samples
+        shape = (n_bat, n_sam, n_dim)    
+        return mu3 + T.exp(ls3) * self.rng.normal(shape)        
+
 class BernoulliSampleLayer(lasagne.layers.Layer):
     def __init__(self, mean,
                  seed=lasagne.random.get_rng().randint(1, 2147462579),
@@ -36,4 +65,4 @@ class BernoulliSampleLayer(lasagne.layers.Layer):
         return input_shape
 
     def get_output_for(self, mu, **kwargs):
-        return self._srng.binomial(size=mu.shape, p=mu, dtype=mu.dtype)        
+        return self._srng.binomial(size=mu.shape, p=mu, dtype=mu.dtype)
