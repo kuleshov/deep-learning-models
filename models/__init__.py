c4d2a74cca65980b17efb93aa82f1956bf92545d
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2017-02-05 14:03:21 -0800

    fix small bug in module loading; verified that dcgan works well

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
 from softmax import Softmax
 from mlp import MLP
 from cnn import CNN
-from kcnn import KCNN
 from vae import VAE
 from sbn import SBN
 from adgm import ADGM
 from dadgm import DADGM
 
+# load some models that require the latest version of Lasagne
 try:
   from resnet import Resnet
 except:
@@ -21,3 +21,10 @@ try:
   from ssadgm import SSADGM
 except:
   print 'WARNING: Could not import the SSADGM model'  
+
+# import keras models if we're using the theano backend
+# (the tensorflow backend will conflict with main theano code)
+try:
+  import keras.backend as K
+  if K.backend() == 'theano':
+    from kcnn import KCNN
