fix gradient estimator in vae

models/vae.py
@@ -5,25 +5,95 @@ import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
+from lasagne.layers import batch_norm
 
 from model import Model
 
 from layers import GaussianSampleLayer
+from layers import extras as nn
 from distributions import log_bernoulli, log_normal, log_normal2
 
 # ----------------------------------------------------------------------------
 
 class VAE(Model):
   """Variational Autoencoder with Gaussian visible and latent variables"""
-  def __init__(self, n_dim, n_out, n_chan=1, n_superbatch=12800, model='bernoulli',
+  def __init__(self, n_dim, n_out, n_chan=1, n_batch=128, n_superbatch=12800, model='bernoulli',
                 opt_alg='adam', opt_params={'lr' : 1e-3, 'b1': 0.9, 'b2': 0.99}):
     # save model that wil be created
     self.model = model
+    self.n_batch = n_batch
 
     # invoke parent constructor
     Model.__init__(self, n_dim, n_chan, n_out, n_superbatch, opt_alg, opt_params)
-  
+
   def create_model(self, X, Y, n_dim, n_out, n_chan=1):
+    if self.model == 'svhn':
+      return self.create_svhn_model(X, Y, n_dim, n_out, n_chan)
+    else:
+      return self.create_std_model(X, Y, n_dim, n_out, n_chan)
+  
+  def create_svhn_model(self, X, Y, n_dim, n_out, n_chan=1):
+    # params
+    n_lat = 100 # latent stochastic variabels
+    n_out = n_dim * n_dim * n_chan # total dimensionality of output
+    hid_nl = lasagne.nonlinearities.rectified
+
+    # create the encoder network
+    l_q_in = lasagne.layers.InputLayer(shape=(None, n_chan, n_dim, n_dim), 
+                                     input_var=X)
+
+    l_q_conv1 = lasagne.layers.Conv2DLayer(
+        l_q_in, num_filters=128, filter_size=(5, 5), stride=2,
+        nonlinearity=lasagne.nonlinearities.leaky_rectify(0.2),
+        pad='same', W=lasagne.init.Normal(5e-2))
+
+    l_q_conv2 = nn.batch_norm(lasagne.layers.Conv2DLayer(
+        l_q_conv1, num_filters=256, filter_size=(5, 5), stride=2,
+        nonlinearity=lasagne.nonlinearities.leaky_rectify(0.2),
+        pad='same', W=lasagne.init.Normal(5e-2)), g=None)
+
+    l_q_conv3 = nn.batch_norm(lasagne.layers.Conv2DLayer(
+        l_q_conv2, num_filters=512, filter_size=(5, 5), stride=2,
+        nonlinearity=lasagne.nonlinearities.leaky_rectify(0.2),
+        pad='same', W=lasagne.init.Normal(5e-2)), g=None)
+
+    l_q_mu = lasagne.layers.DenseLayer(
+        l_q_conv3, num_units=n_lat, nonlinearity=None,
+        W=lasagne.init.Normal(5e-2))
+
+    l_q_logsigma = lasagne.layers.DenseLayer(
+        l_q_conv3, num_units=n_lat, nonlinearity=None,
+        W=lasagne.init.Normal(5e-2))
+
+    # create the decoder network
+    l_p_z = GaussianSampleLayer(l_q_mu, l_q_logsigma)
+
+    l_p_hid1 = nn.batch_norm(lasagne.layers.DenseLayer(
+        l_p_z, num_units=4*4*512, nonlinearity=hid_nl, 
+        W=lasagne.init.Normal(5e-2)), g=None)
+    l_p_hid1 = lasagne.layers.ReshapeLayer(l_p_hid1, (-1, 512, 4, 4))
+    
+    l_p_hid2 = nn.batch_norm(nn.Deconv2DLayer(l_p_hid1, 
+      (self.n_batch,256,8,8), (5,5), W=lasagne.init.Normal(0.05), 
+      nonlinearity=hid_nl), g=None)
+
+    l_p_hid3 = nn.batch_norm(nn.Deconv2DLayer(l_p_hid2, 
+      (self.n_batch,128,16,16), (5,5), W=lasagne.init.Normal(0.05), 
+      nonlinearity=hid_nl), g=None)
+
+    l_p_mu = nn.weight_norm(nn.Deconv2DLayer(l_p_hid3, 
+      (self.n_batch,3,32,32), (5,5), W=lasagne.init.Normal(0.05), 
+      nonlinearity=lasagne.nonlinearities.sigmoid), train_g=True, init_stdv=0.1)
+
+    l_p_logsigma = nn.weight_norm(nn.Deconv2DLayer(l_p_hid3, 
+      (self.n_batch,3,32,32), (5,5), W=lasagne.init.Normal(0.05), 
+      nonlinearity=None), train_g=True, init_stdv=0.1)
+
+    l_sample = GaussianSampleLayer(l_p_mu, l_p_logsigma)
+
+    return l_p_mu, l_p_logsigma, l_q_mu, l_q_logsigma, l_sample, l_p_z
+
+  def create_std_model(self, X, Y, n_dim, n_out, n_chan=1):
     # params
     n_lat = 200 # latent stochastic variabels
     n_hid = 500 # size of hidden layer in encoder/decoder
@@ -77,6 +147,7 @@ class VAE(Model):
 
     return l_p_mu, l_p_logsigma, l_q_mu, l_q_logsigma, l_sample, l_p_z
 
+
   def create_objectives(self, deterministic=False):
     return self.create_objectives_analytic(deterministic)
 
@@ -110,93 +181,7 @@ class VAE(Model):
 
     # we don't use the spearate accuracy metric right now
     return loss, -kl_div
-
-  def create_objectives_elbo(self, deterministic=False):
-    """ELBO objective without the analytic expectation trick"""
-    # load network input
-    X = self.inputs[0]
-    x = X.flatten(2)
-
-    # load network output
-    if self.model == 'bernoulli':
-      q_mu, q_logsigma, p_mu, z \
-           = lasagne.layers.get_output(self.network[2:], deterministic=deterministic)
-    elif self.model == 'gaussian':
-      raise NotImplementedError()
-
-    # entropy term
-    log_qz_given_x = log_normal2(z, q_mu, q_logsigma).sum(axis=1)
-
-    # expected p(x,z) term
-    z_prior_sigma = T.cast(T.ones_like(q_logsigma), dtype=theano.config.floatX)
-    z_prior_mu = T.cast(T.zeros_like(q_mu), dtype=theano.config.floatX)
-    log_pz = log_normal(z, z_prior_mu,  z_prior_sigma).sum(axis=1)     
-    log_px_given_z = log_bernoulli(x, p_mu).sum(axis=1)
-    log_pxz = log_pz + log_px_given_z
-
-    elbo = (log_pxz - log_qz_given_x).mean()
-
-    # we don't use the spearate accuracy metric right now
-    return -elbo, -log_qz_given_x.mean()  
-
-  def create_gradients(self, loss, deterministic=False):
-    from theano.gradient import disconnected_grad as dg
-
-    # load network input
-    X = self.inputs[0]
-    x = X.flatten(2)
-
-    # load network output
-    if self.model == 'bernoulli':
-      q_mu, q_logsigma, p_mu, z \
-           = lasagne.layers.get_output(self.network[2:], deterministic=deterministic)
-    elif self.model == 'gaussian':
-      raise NotImplementedError()
-
-    # load params
-    p_params, q_params = self._get_net_params()
-
-    # entropy term
-    log_qz_given_x = log_normal2(z, q_mu, q_logsigma).sum(axis=1)
-
-    # expected p(x,z) term
-    z_prior_sigma = T.cast(T.ones_like(q_logsigma), dtype=theano.config.floatX)
-    z_prior_mu = T.cast(T.zeros_like(q_mu), dtype=theano.config.floatX)
-    log_pz = log_normal(z, z_prior_mu, z_prior_sigma).sum(axis=1)     
-    log_px_given_z = log_bernoulli(x, p_mu).sum(axis=1)
-    log_pxz = log_pz + log_px_given_z
-
-    # compute learning signals
-    l = log_pxz - log_qz_given_x 
-    # l_avg, l_std = l.mean(), T.maximum(1, l.std())
-    # c_new = 0.8*c + 0.2*l_avg
-    # v_new = 0.8*v + 0.2*l_std
-    # l = (l - c_new) / v_new
-  
-    # compute grad wrt p
-    p_grads = T.grad(-log_pxz.mean(), p_params)
-
-    # compute grad wrt q
-    # q_target = T.mean(dg(l) * log_qz_given_x)
-    # q_grads = T.grad(-0.2*q_target, q_params) # 5x slower rate for q
-    log_qz_given_x = log_normal2(dg(z), q_mu, q_logsigma).sum(axis=1)
-    q_target = T.mean(dg(l) * log_qz_given_x)
-    q_grads = T.grad(-0.2*q_target, q_params) # 5x slower rate for q
-    # q_grads = T.grad(-l.mean(), q_params) # 5x slower rate for q
-
-    # # compute grad of cv net
-    # cv_target = T.mean(l**2)
-    # cv_grads = T.grad(cv_target, cv_params)
-
-    # combine and clip gradients
-    clip_grad = 1
-    max_norm = 5
-    grads = p_grads + q_grads
-    mgrads = lasagne.updates.total_norm_constraint(grads, max_norm=max_norm)
-    cgrads = [T.clip(g, -clip_grad, clip_grad) for g in mgrads]
-
-    return cgrads  
-
+    
   def _get_net_params(self):
     # load network output
     if self.model == 'bernoulli':